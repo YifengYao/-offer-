87e42d418566ad0ab4d84207c05b6bc37f753cbf
yao yao <2713177204@qq.com>
2019-06-28 09:58:51 +0800

    新创建

--- 06_PrintListInReversingly/06_PrintListInReversingly.cpp
+++ 06_PrintListInReversingly/06_PrintListInReversingly.cpp
@@ -6,8 +6,9 @@
 // 面试题6：从尾到头打印链表
 // 题目：输入一个链表的头结点，从尾到头反过来打印出每个结点的值。
 
-#include "..\Utilities\List.h"
+#include "..\Utilities\List.cpp"
 #include <stack>
+#include <cstdio>
 
 void PrintListReversingly_Iteratively(ListNode* pHead)
 {
